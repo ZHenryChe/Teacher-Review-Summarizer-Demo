9777fa72e0f7b84dac8288db3746660754c1ac4d
ZHenryChe <162820875+ZHenryChe@users.noreply.github.com>
2024-12-01 21:37:41 -0500

    Update txt_file.py

--- webpages/txt_file.py
+++ webpages/txt_file.py
@@ -1,6 +1,8 @@
 import streamlit as st
-import wx
+import time
 from transformers import pipeline
+import tempfile
+import os
 import nltk
 import pandas as pd
 
@@ -12,28 +14,33 @@ sentiment_model = pipeline(model="ZephyrUtopia/ratemyprofessors-reviews-sentimen
 #Model for keyword extraction
 keywordextract = pipeline("text2text-generation", model="ZephyrUtopia/keyword-summarizer-10000-v1", max_new_tokens=512)
 
-
 #text display
 st.write("""
         # Please upload a file to summarize.
          """)
 
 #file selector
-if st.button('Browse'):
-    dialog = wx.DirDialog(None, 'Select a folder:', style=wx.DD_DEFAULT_STYLE | wx.DD_NEW_DIR_BUTTON)
-    if dialog.ShowModal() == wx.ID_OK:
-        folder_path = dialog.GetPath() # folder_path will contain the path of the folder you have selected as string
+uploaded_file = st.file_uploader("Browse")
 
-revs = open(folder_path, 'r')
+while uploaded_file == None:
+    time.sleep(1)
+
+if uploaded_file:
+        temp_dir = tempfile.mkdtemp()
+        path = os.path.join(temp_dir, uploaded_file.name)
+        with open(path, "wb") as f:
+                f.write(uploaded_file.getvalue())
+
+revs = open(path, 'r')
 
 revs_split = revs.read().split('\n\n')  #comments to review
 
 #dictionary to visualize final results
 diction = {"Positive": [], 'Negative': []}
 
-for rev in revs_split:
+with st.spinner('Extracting keywords...'):
 
-    with st.spinner('Extracting keywords...'):
+    for rev in revs_split:
 
         if rev:
             # Get the model prediction
@@ -62,7 +69,7 @@ for rev in revs_split:
                 end_pos = keywords.find("'", start_pos+1)
 
                 # Adding found word to dictionary
-                if label == 0:
+                if label == 'LABEL_0':
                     diction['Negative'].append(keywords[start_pos:end_pos+1])
                 else:
                     diction['Positive'].append(keywords[start_pos:end_pos+1])
@@ -71,8 +78,8 @@ for rev in revs_split:
                 keywords = keywords[end_pos+1:]
 
 
-#table to visualize results
-df = pd.DataFrame(data = diction)
+#table to visualize results, orient+transpose to solve values not equal length problem
+df = pd.DataFrame.from_dict(data = diction, orient = 'index').transpose()
 
 # Display keywords
 st.table(df)
